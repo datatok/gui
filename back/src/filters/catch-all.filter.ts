c4d11cacab3f5702b19da78c510eb32d708abb84
Thomas Decaux <ebuildy@gmail.com>
2022-11-05 20:31:19 +0100

    feat(back): improve error logging

--- back/src/filters/catch-all.filter.ts
+++ back/src/filters/catch-all.filter.ts
@@ -1,37 +1,58 @@
+import { NoSuchBucket } from '@aws-sdk/client-s3';
 import {
-    ExceptionFilter,
-    Catch,
-    ArgumentsHost,
-    HttpException,
-    HttpStatus,
-  } from '@nestjs/common';
-  import { HttpAdapterHost } from '@nestjs/core';
-  
-  @Catch()
-  export class AllExceptionsFilter implements ExceptionFilter {
-    constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
-  
-    catch(exception: unknown, host: ArgumentsHost): void {
-      // In certain situations `httpAdapter` might not be available in the
-      // constructor method, thus we should resolve it here.
-      const { httpAdapter } = this.httpAdapterHost;
-  
-      const ctx = host.switchToHttp();
-  
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  formatMessage({ statusCode, reason, trace, path }) {
+    return {
+      statusCode,
+      timestamp: new Date().toISOString(),
+      reason,
+      trace,
+      path,
+    };
+  }
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    // In certain situations `httpAdapter` might not be available in the
+    // constructor method, thus we should resolve it here.
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const path = httpAdapter.getRequestUrl(ctx.getRequest());
+    let responseBody;
+
+    console.error(exception);
+
+    if (exception instanceof NoSuchBucket) {
+      responseBody = this.formatMessage({
+        statusCode: 500,
+        reason: `bucket not found`,
+        trace: exception instanceof Error ? exception.stack : '',
+        path,
+      });
+    } else {
       const httpStatus =
         exception instanceof HttpException
           ? exception.getStatus()
           : HttpStatus.INTERNAL_SERVER_ERROR;
-  
-      const responseBody = {
+
+      responseBody = this.formatMessage({
         statusCode: httpStatus,
-        timestamp: new Date().toISOString(),
         reason: exception instanceof Error ? exception.message : '',
         trace: exception instanceof Error ? exception.stack : '',
-        path: httpAdapter.getRequestUrl(ctx.getRequest()),
-      };
-  
-      httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+        path,
+      });
     }
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, responseBody.statusCode);
   }
-  
+}
